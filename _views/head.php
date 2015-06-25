326bf3d51b1a6056e0c07be80bbc32da0ea1d237
jephmann <jephmann@gmail.com>
2015-06-25 00:42:40 -0500

    _views tweaks

--- _views/head.php
+++ _views/head.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">        
@@ -11,8 +6,3 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
