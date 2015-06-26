cda747348fb74874c9b33dda06c9d88104db8cb1
jephmann <jephmann@gmail.com>
2015-06-25 22:04:52 -0500

    Adding provision for City of Chicago API

---
+++ _classes/API_CityOfChicago.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Description of API_CityOfChicago
+ *
+ * @author Jeffrey
+    */
+
+   class API_CityOfChicago extends API {
+
+           private $key    = "YourKeyHere";
+           private $url    = "BaseURLofAPI";
+
+   }
---
+++ cityofchicago/index.php
@@ -0,0 +1,30 @@
+<?php
+    require_once '../_inc/first.php';
+    $objPage = new Page;
+    $objPage->subtitle = "City of Chicago";
+    $objPage->path = "../";
+
+    /*
+     * Additional code if necessary
+     */
+    
+    /*
+     * HTML BEGIN
+     */
+    require_once "{$objPage->path}_views/head.php";
+    require_once "{$objPage->path}_views/header.php";
+?>
+        <article>
+            <h3>Hello</h3>
+            <p>etc.</p>
+            <ul>
+                <li><a href="<?php echo $objPage->path ?>">HOME</a></li>
+                <li><a href="">One of many City of Chicago links</a></li>
+            </ul>
+        </article>
+<?php
+    require_once "{$objPage->path}_views/footer.php";
+    require_once "{$objPage->path}_views/foot.php";
+    /*
+     * HTML END
+     */
