31be4e84a38010b40688df19568ba28e45927159
jephmann <jephmann@gmail.com>
2015-07-05 01:27:23 -0500

    sortable CityOfChicago table via angularjs and fontawesome

--- _js/apilab.js
+++ _js/apilab.js
@@ -6,11 +6,17 @@
                 'ctrlAPILab', function($scope, $http){
 		$scope.text_h1 = "City of Chicago"
 		$scope.text_h2 = "City Employees"
+        
+                $scope.sortType = 'department';
+                $scope.sortReverse = false;
+                
 		$http.get("https://data.cityofchicago.org/resource/xzkq-xp2w.json").success(
                     function(response){
                         $scope.cityEmployees = response
                     }
                 )
+        
+        
             }
         )
     }
--- cityofchicago/angular_test.php
+++ cityofchicago/angular_test.php
@@ -17,25 +17,54 @@
             echo $objPage->title;
             ?></title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     </head>
     <body ng-app="apilab">
             <div class="container">
                 <div ng-controller="ctrlAPILab">
                     <h1>{{text_h1}}</h1>                    
                     <h2>{{text_h2}}</h2>
+                    <div class="alert alert-info">
+                        <p>Sort Type: {{ sortType }}</p>
+                        <p>Sort Reverse: {{ sortReverse }}</p>
+                    </div>
                     <div class="table-responsive"> 
                         <table class="table table-condensed table-striped table-bordered table-hover">
                             <caption>{{text_h1}} | {{text_h2}}</caption>
                             <thead>
                                 <tr>
-                                    <th>Department</th>
-                                    <th>Job Title</th>
-                                    <th>Name</th>
-                                    <th>Salary</th>
+                                    <th>
+                                        <a  href="#" ng-click="sortType = 'department'; sortReverse = !sortReverse">
+                                        Department
+                                        <span ng-show="sortType == 'department' && !sortReverse" class="fa fa-caret-down"></span>
+                                        <span ng-show="sortType == 'department' && sortReverse" class="fa fa-caret-up"></span>
+                                        </a>
+                                    </th>
+                                    <th>
+                                        <a  href="#" ng-click="sortType = 'job_titles'; sortReverse = !sortReverse">
+                                        Job Title
+                                        <span ng-show="sortType == 'job_titles' && !sortReverse" class="fa fa-caret-down"></span>
+                                        <span ng-show="sortType == 'job_titles' && sortReverse" class="fa fa-caret-up"></span>
+                                        </a>
+                                    </th>
+                                    <th>
+                                        <a  href="#" ng-click="sortType = 'name'; sortReverse = !sortReverse">
+                                        Name
+                                        <span ng-show="sortType == 'name' && !sortReverse" class="fa fa-caret-down"></span>
+                                        <span ng-show="sortType == 'name' && sortReverse" class="fa fa-caret-up"></span>
+                                        </a>
+                                    </th>
+                                    <th>
+                                        <a  href="#" ng-click="sortType = 'employee_annual_salary'; sortReverse = !sortReverse">
+                                        Salary
+                                        <span ng-show="sortType == 'employee_annual_salary' && !sortReverse" class="fa fa-caret-down"></span>
+                                        <span ng-show="sortType == 'employee_annual_salary' && sortReverse" class="fa fa-caret-up"></span>
+                                        </a>
+                                    </th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr ng-repeat="employee in cityEmployees | orderBy:['department', 'job_titles', '-employee_annual_salary']">
+                                <tr ng-repeat="employee in cityEmployees | orderBy:sortType:sortReverse">
                                         <td>{{employee.department}}</td>
                                         <td>{{employee.job_titles}}</td>
                                         <td>{{employee.name}}</td>
